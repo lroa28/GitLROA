8723165c508329414f94121845a31bfbabb4f3ec
Lucrecia Roa <lucreciaroa@hotmail.com>
2021-10-30 22:14:48 -0300

    Entrega1 Curso JS

---
+++ pages/js/clase1AJs.js
@@ -0,0 +1,22 @@
+/* Archivo JS linkeado en contacto.html */
+
+/* Sugerencia: Usamos prompt() para solicitar datos al usuario y console.log() o alert() para mostrar el 
+resultado de las operaciones realizadas con esos datos. Si vas a sumar una entrada tene en cuenta que tenes que 
+parsearla antes. Usando parseInt() o parseFloat(). 
+
+Desafio 1-A: Pedir nombre mediante prompt y mostrarlo en consola junto con algún texto de saludo. 
+Ejemplo:  ¡Hola, Juan!*/
+
+let nombre = prompt("Please enter your name (Por favor, ingrese su nombre):");
+console.log("Hello," + nombre + "!");
+
+/* Desafio 1-B: Pedir un número mediante prompt, parsearlo, sumarlo a otro que se encuentre almacenado en una 
+variable y luego mostrar el resultado en consola.*/
+
+var numero;
+numero = 1; 
+
+let descuentodeseado = prompt("Ingrese el descuento que le gustaría obtener en su compra (del 1 al 100):");
+let descuento = parseInt(descuentodeseado) * numero;
+console.log("El porcentaje de descuento que desea es: " + descuento + "%" );
+alert("El porcentaje de descuento que desea es: " + descuento + "%");
---
+++ pages/js/clase1BJs.js
@@ -0,0 +1,11 @@
+/* Archivo JS linkeado en search.html */
+
+/* Desafio 1-C: Pedir un texto mediante prompt, luego otro, concatenarlos y mostrarlo en un alerta.*/
+
+let texto1 = prompt("Please ingrese una prenda no encontrada en la page:");
+let texto2 = prompt("Please ingrese el color de la prenda no encontrada en la page:");
+
+var texto3;
+texto3= texto1 + texto2; 
+
+alert( texto3 )
